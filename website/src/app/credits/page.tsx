f7dfe6817dfa7608f87cab6cc2b8bebad2a46c6f
MekaSid <sidmekarao@gmail.com>
2024-11-08 13:40:08 +0100

    initial commit

---
+++ website/src/app/credits/page.tsx
@@ -0,0 +1,91 @@
+// page.tsx
+
+import React from 'react';
+import styles from './credits.module.css';
+
+const Credits: React.FC = () => {
+  return (
+    <div className={styles.creditsContainer}>
+      <h1 className={styles.title}>Credits</h1>
+      <ul className={styles.creditsList}>
+        <li className={styles.creditsItem}>
+          <a href="https://www.flaticon.com/free-icons/python" title="python icons" target="_blank" rel="noopener noreferrer" className={styles.creditsLink}>
+            Python icons created by Freepik - Flaticon
+          </a>
+        </li>
+        <li className={styles.creditsItem}>
+          <a href="https://www.flaticon.com/free-icons/sm" title="sm icons" target="_blank" rel="noopener noreferrer" className={styles.creditsLink}>
+            Sm icons created by Techno Icons - Flaticon
+          </a>
+        </li>
+        <li className={styles.creditsItem}>
+          <a href="https://www.flaticon.com/free-icons/react" title="react icons" target="_blank" rel="noopener noreferrer" className={styles.creditsLink}>
+            React icons created by Pixel perfect - Flaticon
+          </a>
+        </li>
+        <li className={styles.creditsItem}>
+          <a href="https://www.flaticon.com/free-icons/database" title="database icons" target="_blank" rel="noopener noreferrer" className={styles.creditsLink}>
+            Database icons created by juicy_fish - Flaticon
+          </a>
+        </li>
+        <li className={styles.creditsItem}>
+          <a href="https://www.flaticon.com/free-icons/java" title="java icons" target="_blank" rel="noopener noreferrer" className={styles.creditsLink}>
+            Java icons created by Freepik - Flaticon
+          </a>
+        </li>
+        <li className={styles.creditsItem}>
+          <a href="https://www.flaticon.com/free-icons/javascript" title="javascript icons" target="_blank" rel="noopener noreferrer" className={styles.creditsLink}>
+            Javascript icons created by Freepik - Flaticon
+          </a>
+        </li>
+        <li className={styles.creditsItem}>
+          <a href="https://www.flaticon.com/free-icons/typescript" title="typescript icons" target="_blank" rel="noopener noreferrer" className={styles.creditsLink}>
+            Typescript icons created by Freepik - Flaticon
+          </a>
+        </li>
+        <li className={styles.creditsItem}>
+          <a href="https://www.flaticon.com/free-icons/restaurant" title="restaurant icons" target="_blank" rel="noopener noreferrer" className={styles.creditsLink}>
+            Restaurant icons created by Freepik - Flaticon
+          </a>
+        </li>
+        <li className={styles.creditsItem}>
+          <a href="https://www.flaticon.com/free-icons/strategy" title="strategy icons" target="_blank" rel="noopener noreferrer" className={styles.creditsLink}>
+            Strategy icons created by DinosoftLabs - Flaticon
+          </a>
+        </li>
+        <li className={styles.creditsItem}>
+          <a href="https://www.flaticon.com/free-icons/chess" title="chess icons" target="_blank" rel="noopener noreferrer" className={styles.creditsLink}>
+            Chess icons created by SBTS2018 - Flaticon
+          </a>
+        </li>
+        <li className={styles.creditsItem}>
+          <a href="https://www.flaticon.com/free-icons/paper-plane" title="paper plane icons" target="_blank" rel="noopener noreferrer" className={styles.creditsLink}>
+            Paper plane icons created by Freepik - Flaticon
+          </a>
+        </li>
+        <li className={styles.creditsItem}>
+          <a href="https://www.flaticon.com/free-icons/burrito" title="burrito icons" target="_blank" rel="noopener noreferrer" className={styles.creditsLink}>
+            Burrito icons created by Freepik - Flaticon
+          </a>
+        </li>
+        <li className={styles.creditsItem}>
+          <a href="https://www.flaticon.com/free-icons/security" title="security icons" target="_blank" rel="noopener noreferrer" className={styles.creditsLink}>
+            Security icons created by Freepik - Flaticon
+          </a>
+        </li>
+        <li className={styles.creditsItem}>
+          <a href="https://www.flaticon.com/free-icons/dumbell" title="Dumbell icons" target="_blank" rel="noopener noreferrer" className={styles.creditsLink}>
+            Dumbell icons created by kmg design - Flaticon
+          </a>
+        </li>
+        <li className={styles.creditsItem}>
+          <a href="https://www.flaticon.com/free-icons/personal-development" title="personal development icons" target="_blank" rel="noopener noreferrer" className={styles.creditsLink}>
+            Personal development icons created by Maxim Basinski Premium - Flaticon
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Credits;
